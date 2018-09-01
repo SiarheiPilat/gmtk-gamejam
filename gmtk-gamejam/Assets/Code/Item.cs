76cca3df14f76feb715777153840ce3fd087c5b1
Siarhei Pilat <spilat12@gmail.com>
2018-09-01 04:20:34 +0200

    implemented drag (without drop yet)

---
+++ gmtk-gamejam/Assets/Code/Item.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Item : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IDragHandler, IEndDragHandler
+{
+
+    private Vector3 sittingPosition;
+
+    void Start()
+    {
+        sittingPosition = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = sittingPosition;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
